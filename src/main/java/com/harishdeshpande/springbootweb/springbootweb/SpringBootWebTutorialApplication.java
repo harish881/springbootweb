fa06494cfac81809a0f43c3d67413fd4e16a623c
harish881 <mailharishdeshpande@gmail.com>
2025-06-02 11:06:14 +0530

    first commit

---
+++ src/main/java/com/harishdeshpande/springbootweb/springbootweb/SpringBootWebTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.harishdeshpande.springbootweb.springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harishdeshpande/springbootweb/springbootweb/SpringBootWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harishdeshpande.springbootweb.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
